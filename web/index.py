5c6083fe638f6ef6b10ecc2366f4a702aa307dec
David Link <dvlink@gmail.com>
2016-04-30 02:00:07 -0400

    added several new pages

--- web/index.py
+++ web/index.py
@@ -8,6 +8,13 @@ class Index(HtmlPage):
     def __init__(self, name='United LA Home Improvement'):
         HtmlPage.__init__(self, name)
         self.style_sheets = ['css/main.css']
+        self.javascript_src.append('js/main.js')
+        self.page = None
+        
+    def process(self):
+        HtmlPage.process(self)
+        if 'menu_choice' in self.form:
+            self.page = self.form['menu_choice'].value
 
     def getHtmlContent(self):
         return \
@@ -19,7 +26,21 @@ class Index(HtmlPage):
         return open('header.html','r').read()
 
     def getBody(self):
-        return open('body.html','r').read()
+        return open('body.html','r').read() \
+            % (self.getMenu(), self.getLeftSide())
+
+    def getMenu(self):
+        return open('menu.html', 'r').read()
+
+    def getLeftSide(self):
+        page = self.page
+
+        if self.page:
+            file = 'leftside-%s.html' % self.page
+        else:
+            file = 'leftside-home.html'
+
+        return open(file, 'r').read()
 
     def getFooter(self):
         return ''
---
+++ web/js/main.js
@@ -0,0 +1,4 @@
+function menuClick(choice) {
+    document.form1.menu_choice.value = choice;
+    document.form1.submit();
+}
